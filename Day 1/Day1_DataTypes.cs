47bd2aa57baa3758383747967c8154d5658bfd25
Riadh <riadh.dhaoui@rub.de>
2021-01-10 17:47:52 +0100

    commit

---
+++ Day 1/Day1_DataTypes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution {
+	
+    static void Main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        string s = "HackerRank ";
+
+      
+        int j;    // Declaration of second Integer, Double, and String variables.
+        double e;
+        string NewString;
+        j = Convert.ToInt32(Console.ReadLine());
+        e = Convert.ToDouble(Console.ReadLine());
+        NewString = Console.ReadLine();
+        // Print the sum of both integer variables on a new line.
+        Console.WriteLine(j + i);
+        Console.WriteLine("{0:F1}", d + e);
+        Console.WriteLine(s + NewString);            
+        // Print the sum of the double variables on a new line.
+        
+        // Concatenate and print the String variables on a new line
+        // The 's' variable above should be printed first.
+
+    }
+}
